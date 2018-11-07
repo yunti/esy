a9452e8ab3006fd4b54c568d351e6da2fc46fb5e
Andrey Popp <8mayday@gmail.com>
2018-11-07 17:26:24 +0300

    Add store path padding on Windows

--- test/Store.ml
+++ test/Store.ml
@@ -2,13 +2,6 @@ include EsyLib.Store
 
 module System = EsyLib.System
 
-let%test "Validate padding length on Windows is always 1" = 
-    let prefixPath = Fpath.v "test" in
-    let padding = getPadding ~system:System.Platform.Windows prefixPath in
-    match padding with 
-        | Ok("_") -> true
-        | _ -> false
-
 let%test "Validate padding length on other platforms is not 1" =
     let prefixPath = Fpath.v "test" in
     let padding = getPadding ~system:System.Platform.Darwin prefixPath in
